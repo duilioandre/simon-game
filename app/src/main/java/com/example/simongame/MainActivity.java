de0302a8b406c21d8172bc1e6971a6bfb69b00ad
andre <duilioandrecastro@gmail.com>
2022-12-04 20:41:23 +0100

    data binding implemented + test button

--- app/src/main/java/com/example/simongame/MainActivity.java
+++ app/src/main/java/com/example/simongame/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.simongame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.simongame.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.buttonTestBinding1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("button binding test", "button clicked");
+            }
+        });
     }
 }
