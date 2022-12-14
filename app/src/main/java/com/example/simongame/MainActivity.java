3ca57db7d23b5f39006ddc8e727846a9dd18621c
andre <duilioandrecastro@gmail.com>
2022-12-14 23:59:49 +0100

    feat: game logic test

--- app/src/main/java/com/example/simongame/MainActivity.java
+++ app/src/main/java/com/example/simongame/MainActivity.java
@@ -25,55 +25,84 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        setButtonIds(buttonIds);
-
-        binding.buttonStart.setOnClickListener(view -> {
-
-//            buttonSequenceGenerated.add("2131230819");
-//            buttonSequenceGenerated.add("2131231202");
-//            buttonSequenceGenerated.add("2131231205");
-//            buttonSequenceGenerated.add("2131231203");
-            generatedSequenceOfButtons(buttonIds, buttonSequenceGenerated);
-
-            selectButtonInGeneratedSequence(cursore,  buttonSequenceGenerated);
-
-
+        binding.buttonTestBinding1.setOnClickListener(view -> {
+            buttonSequenceUser.add(String.valueOf(binding.buttonTestBinding1.getId()));
+            System.out.println(binding.buttonTestBinding1.getId());
         });
 
-        binding.buttonTestBinding1.setOnClickListener(view -> buttonSequenceUser.add(String.valueOf(binding.buttonTestBinding1.getId())));
+        binding.buttonTestBinding2.setOnClickListener(view -> {
+            buttonSequenceUser.add(String.valueOf(binding.buttonTestBinding2.getId()));
+            System.out.println(binding.buttonTestBinding2.getId());
+        });
 
-        binding.buttonTestBinding2.setOnClickListener(view -> buttonSequenceUser.add(String.valueOf(binding.buttonTestBinding2.getId())));
+        binding.buttonTestBinding3.setOnClickListener(view -> {
+            buttonSequenceUser.add(String.valueOf(binding.buttonTestBinding3.getId()));
+            System.out.println(binding.buttonTestBinding3.getId());
+        });
 
-        binding.buttonTestBinding3.setOnClickListener(view -> buttonSequenceUser.add(String.valueOf(binding.buttonTestBinding3.getId())));
+        binding.buttonTestBinding4.setOnClickListener(view -> {
+            buttonSequenceUser.add(String.valueOf(binding.buttonTestBinding4.getId()));
+            System.out.println(binding.buttonTestBinding4.getId());
+        });
 
-        binding.buttonTestBinding4.setOnClickListener(view -> buttonSequenceUser.add(String.valueOf(binding.buttonTestBinding4.getId())));
+        binding.buttonStart.setOnClickListener(view -> {
+            binding.buttonStart.setEnabled(false);
+            setButtonIds(buttonIds);
+            play();
+        });
 
     }
 
+    private void play() {
+        generatedSequenceOfButtons(buttonIds, buttonSequenceGenerated);
+//        buttonSequenceGenerated.add("2131230820");
+//        buttonSequenceGenerated.add("2131230821");
+//        buttonSequenceGenerated.add("2131230823");
+//        buttonSequenceGenerated.add("2131230822");
+        selectButtonInGeneratedSequence(cursore,  buttonSequenceGenerated);
+    }
 
     private void selectButtonInGeneratedSequence(int cursore, @NonNull ArrayList<String> buttonSequenceGenerated) {
+        buttonSequenceUser.clear();
         if (cursore < buttonSequenceGenerated.size()) {
             setTimeout(() -> {
-                findViewById(Integer.parseInt(buttonSequenceGenerated.get(cursore))).setAlpha(0.5f);
+                changeButtonAlpha(buttonSequenceGenerated, cursore, 0.5f);
                 setTimeout(() -> {
                     this.runOnUiThread(() -> {
-                        findViewById(Integer.parseInt(buttonSequenceGenerated.get(cursore))).setAlpha(1.0f);
+                        changeButtonAlpha(buttonSequenceGenerated, cursore, 1.0f);
                     });
-                }, 1000);
-                selectButtonInGeneratedSequence(cursore+1, buttonSequenceGenerated);
-            }, 2000);
+                }, 500);
+                selectButtonInGeneratedSequence(cursore + 1, buttonSequenceGenerated);
+            }, 1000);
         }
+        else {
+            setTimeout(() -> checkSequences(buttonSequenceGenerated, buttonSequenceUser), 5000);
+        }
+    }
 
+    private void checkSequences(ArrayList<String> buttonSequenceGenerated, ArrayList<String> buttonSequenceUser) {
+            if (buttonSequenceUser.equals(buttonSequenceGenerated)) {
+                play();
+            } else {
+                System.out.println("NO");
+                System.out.println("g: "+buttonSequenceGenerated);
+                System.out.println("u: "+buttonSequenceUser);
+            }
+
+    }
+
+    private void changeButtonAlpha(ArrayList<String> buttonSequenceGenerated, int cursore, float opacity) {
+        findViewById(Integer.parseInt(buttonSequenceGenerated.get(cursore))).setAlpha(opacity);
     }
 
     private void setButtonIds(@NonNull ArrayList<String> buttonIds) {
-        buttonIds.add("2131230819");
-        buttonIds.add("2131231202");
-        buttonIds.add("2131231203");
-        buttonIds.add("2131231205");
+        buttonIds.add("2131230820");
+        buttonIds.add("2131230821");
+        buttonIds.add("2131230823");
+        buttonIds.add("2131230822");
     }
 
-    private ArrayList<String> generatedSequenceOfButtons(ArrayList<String> buttonIds, ArrayList<String> buttonSequenceGenerated) {
+    private ArrayList<String> generatedSequenceOfButtons(@NonNull ArrayList<String> buttonIds, @NonNull ArrayList<String> buttonSequenceGenerated) {
         Random rand = new Random();
         String randomButtonId = buttonIds.get(rand.nextInt(buttonIds.size()));
         buttonSequenceGenerated.add(randomButtonId);
@@ -90,5 +119,6 @@ public class MainActivity extends AppCompatActivity {
                 System.err.println();
             }
         }).start();
+
     }
 }
